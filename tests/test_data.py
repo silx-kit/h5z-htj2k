626759d5c4c35acf33c1db2bd7219c804c706aa3
Thomas VINCENT <thomas.vincent@esrf.fr>
2026-08-25 09:34:44 +0200

    Add test file for v0 and v1 cd_values

--- tests/test_data.py
+++ tests/test_data.py
@@ -26,3 +26,42 @@ def test_bamboo_hercules(filename: str):
     assert max_abs_error <= expected_max_error, (
         f"Max Error: {max_abs_error} > {expected_max_error}"
     )
+
+
+@pytest.mark.decode_only
+@pytest.mark.parametrize("groupname", ["v0", "v1"])
+def test_versions(groupname: str):
+    with h5py.File(DATA_PATH / "versions.h5") as h5f:
+        ref_data = {}
+        for name, entity in h5f["uncompressed"].items():
+            if isinstance(entity, h5py.Group):
+                ref_data[name] = entity["data"][()]
+            else:
+                ref_data[name] = entity[()]
+
+        for name, entity in h5f[groupname].items():
+            if isinstance(entity, h5py.Group):
+                dataset = entity["data"]
+            else:
+                dataset = entity
+            decompressed_data = dataset[()]
+            expected_rmse = dataset.attrs["RMSE"]
+            expected_max_error = dataset.attrs["MAX_ABS_ERROR"]
+
+            ref_name = name.split("_", 1)[-1]
+            if ref_name.startswith("be_"):
+                ref_name = ref_name[3:]
+            diff = decompressed_data.astype(np.float64) - ref_data[ref_name].astype(
+                np.float64
+            )
+
+            rmse = float(np.sqrt(np.mean(diff * diff)))
+            rmse_tolerance = 0.01 * expected_rmse
+            assert rmse <= expected_rmse + rmse_tolerance, (
+                f"name: {name}, RMSE: {rmse} > {expected_rmse}"
+            )
+
+            max_abs_error = np.max(np.abs(diff))
+            assert max_abs_error <= expected_max_error, (
+                f"name: {name}, Max Error: {max_abs_error} > {expected_max_error}"
+            )
