632ea7095a0f3d38e9f1862642c783151396df12
Thomas VINCENT <thomas.vincent@esrf.fr>
2026-08-25 13:53:05 +0200

    do not use pytest subtests

--- tests/test_data.py
+++ tests/test_data.py
@@ -28,41 +28,43 @@ def test_bamboo_hercules(filename: str):
     )
 
 
+with h5py.File(DATA_PATH / "versions.h5") as h5f:
+    VERSION_TESTS = [
+        f"{group}/{key}" for group in ["v0", "v1"] for key in h5f[group].keys()
+    ]
+
+
 @pytest.mark.decode_only
-@pytest.mark.parametrize("groupname", ["v0", "v1"])
-def test_versions(subtests, groupname: str):
+@pytest.mark.parametrize("h5path", VERSION_TESTS)
+def test_versions(h5path: str):
     with h5py.File(DATA_PATH / "versions.h5") as h5f:
-        ref_data = {}
-        for name, entity in h5f["uncompressed"].items():
-            if isinstance(entity, h5py.Group):
-                ref_data[name] = entity["data"][()]
-            else:
-                ref_data[name] = entity[()]
+        ref_name = h5path.split("_", 1)[-1]
+        if ref_name.startswith("be_"):
+            ref_name = ref_name[3:]
+        ref_entity = h5f["uncompressed"][ref_name]
+        if isinstance(ref_entity, h5py.Group):
+            ref_data = ref_entity["data"][()]
+        else:
+            ref_data = ref_entity[()]
 
-        for name, entity in h5f[groupname].items():
-            with subtests.test(msg=name):
-                if isinstance(entity, h5py.Group):
-                    dataset = entity["data"]
-                else:
-                    dataset = entity
-                decompressed_data = dataset[()]
-                expected_rmse = dataset.attrs["RMSE"]
-                expected_max_error = dataset.attrs["MAX_ABS_ERROR"]
+        entity = h5f[h5path]
+        if isinstance(entity, h5py.Group):
+            dataset = entity["data"]
+        else:
+            dataset = entity
+        decompressed_data = dataset[()]
+        expected_rmse = dataset.attrs["RMSE"]
+        expected_max_error = dataset.attrs["MAX_ABS_ERROR"]
 
-                ref_name = name.split("_", 1)[-1]
-                if ref_name.startswith("be_"):
-                    ref_name = ref_name[3:]
-                diff = decompressed_data.astype(np.float64) - ref_data[ref_name].astype(
-                    np.float64
-                )
+        diff = decompressed_data.astype(np.float64) - ref_data.astype(np.float64)
 
-                rmse = float(np.sqrt(np.mean(diff * diff)))
-                rmse_tolerance = 0.01 * expected_rmse
-                assert rmse <= expected_rmse + rmse_tolerance, (
-                    f"name: {name}, RMSE: {rmse} > {expected_rmse}"
-                )
+        rmse = float(np.sqrt(np.mean(diff * diff)))
+        rmse_tolerance = 0.01 * expected_rmse
+        assert rmse <= expected_rmse + rmse_tolerance, (
+            f"name: {h5path}, RMSE: {rmse} > {expected_rmse}"
+        )
 
-                max_abs_error = np.max(np.abs(diff))
-                assert max_abs_error <= expected_max_error, (
-                    f"name: {name}, Max Error: {max_abs_error} > {expected_max_error}"
-                )
+        max_abs_error = np.max(np.abs(diff))
+        assert max_abs_error <= expected_max_error, (
+            f"name: {h5path}, Max Error: {max_abs_error} > {expected_max_error}"
+        )
